99e4aa525521057345225e8e72e0254d9d506f69
RabiulAsif <rabiulasif02@gmail.com>
2026-01-11 00:14:27 +0600

    Three Tier Architecture Using Generic

---
+++ Intro3TierGeneric/BLL/MapperConfig.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using BLL.DTOs;
+using DAL.EF.Models;
+
+namespace BLL
+{
+    public class MapperConfig
+    {
+        static MapperConfiguration cfg = new MapperConfiguration(cfg =>
+        {
+            cfg.CreateMap<Category, CategoryDTO>().ReverseMap();
+            cfg.CreateMap<Product, ProductDTO>().ReverseMap();
+        });
+        public static Mapper GetMapper()
+        {
+            return new Mapper(cfg);
+        }
+    }
+}
---
+++ Intro3TierGeneric/BLL/Services/CategoryService.cs
@@ -0,0 +1,47 @@
+﻿using BLL.DTOs;
+using DAL.EF.Models;
+using DAL.Repos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class CategoryService
+    {
+        CategoryRepo repo;
+        public CategoryService(CategoryRepo repo)
+        {
+            this.repo = repo;
+        }
+        public List<CategoryDTO> Get()
+        {
+            var data = repo.Get();
+            var mapper = MapperConfig.GetMapper();
+            var ret=mapper.Map<List<CategoryDTO>>(data);
+            return ret;
+        }
+        public CategoryDTO Get(int id)
+        {
+            return MapperConfig.GetMapper().Map<CategoryDTO>(repo.Get(id));
+        }
+        public bool Create(CategoryDTO c)
+        {
+            var mapper = MapperConfig.GetMapper();
+            var data = mapper.Map<Category>(c);
+            return repo.Create(data);
+        }
+        public bool Update(CategoryDTO c)
+        {
+            var mapper = MapperConfig.GetMapper();
+            var data = mapper.Map<Category>(c);
+            return repo.Update(data);
+        }
+        public bool Delete(int id)
+        {
+            return repo.Delete(id);
+        }
+    }
+}
